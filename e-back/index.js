6fb6eb9138a7d5d113056ad8cb3a85f088c45e1b
Yehia Sakr <yehiasakr4676@gmail.com>
2025-04-17 13:02:29 +0200

    implement User

--- e-back/index.js
+++ e-back/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const userRoutes = require("./src/routes/userRoutes");
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+
+app.use("/api/users", userRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
